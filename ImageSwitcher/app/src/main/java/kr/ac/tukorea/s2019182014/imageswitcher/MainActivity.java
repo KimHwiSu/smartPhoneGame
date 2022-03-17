6af6a07562165f26b982b7fab56ee47e999ec188
KimHwiSu <58390829+KimHwiSu@users.noreply.github.com>
2022-03-17 19:23:54 +0900

    ImageSwitcher 완성

--- ImageSwitcher/app/src/main/java/kr/ac/tukorea/s2019182014/imageswitcher/MainActivity.java
+++ ImageSwitcher/app/src/main/java/kr/ac/tukorea/s2019182014/imageswitcher/MainActivity.java
@@ -1,10 +1,14 @@
 package kr.ac.tukorea.s2019182014.imageswitcher;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,22 +23,36 @@ public class MainActivity extends AppCompatActivity {
     };
     int page;
 
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setPage(1);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        pref = getSharedPreferences("pref", MainActivity.MODE_PRIVATE);
+        editor = pref.edit();
+
+        setPage(pref.getInt("savedPage", 1));
     }
+
 
     public void onBtnPrev(View view){
-
         Log.d(TAG, "Prev Button Press");
         setPage(page-1);
+
+        findViewById(R.id.nextButton).setEnabled(true);
     }
+
 
     public void onBtnNext(View view){
         Log.d(TAG, "Next Button Press");
         setPage(page+1);
+
+        findViewById(R.id.prevButton).setEnabled(true);
+
     }
 
     private void setPage(int newPage) {
@@ -46,9 +64,16 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = findViewById(R.id.pageText);
         tv.setText(text);
 
-
-
         ImageView iv =findViewById(R.id.contentImageView);
         iv.setImageResource(RES_IDS[page-1]);
+
+        if(page == 1){
+            findViewById(R.id.prevButton).setEnabled(false);
+        }
+        else if(page == RES_IDS.length){
+            findViewById(R.id.nextButton).setEnabled(false);
+        }
+        editor.putInt("savedPage", page);
+        editor.apply();
     }
 }
